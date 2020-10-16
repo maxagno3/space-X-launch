51a67c9abd89751feebf58b303f12489292772f2
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2020-10-16 21:23:59 +0530

    add modal

--- src/components/FilterByDate.js
+++ src/components/FilterByDate.js
@@ -4,36 +4,38 @@ import "react-datepicker/dist/react-datepicker.css";
 
 function FilterByDate() {
   return (
-    <div className="flex flex-row mb-1 sm:mb-0">
-      <div className="relative mr-3 text-white">
-        <p>Start Date</p>
-        <ReactDatePicker
-          selectsStart
-          isClearable
-          dateFormat="yyyy/MM/dd"
-          showYearDropDown
-          showMonthDropDown
-          closeOnScroll={true}
-          className="cursor-pointer bg-teal-200 text-black p-2"
-        />
+    <>
+      <div className="flex flex-row mb-1 sm:mb-0">
+        <div className="relative mr-3">
+          <p>Start Date</p>
+          <ReactDatePicker
+            selectsStart
+            isClearable
+            dateFormat="yyyy/MM/dd"
+            showYearDropDown
+            showMonthDropDown
+            closeOnScroll={true}
+            className="cursor-pointer bg-teal-200 text-black p-2"
+          />
+        </div>
+        <div className="relative">
+          <p>End Date</p>
+          <ReactDatePicker
+            selectsEnd
+            isClearable
+            minDate={new Date()}
+            dateFormat="yyyy/MM/dd"
+            showYearDropDown
+            showMonthDropDown
+            closeOnScroll={true}
+            className="cursor-pointer bg-teal-200 text-black p-2"
+          />
+          <button className="ml-10 text-white p-2 bg-red-300">
+            Filter Dates
+          </button>
+        </div>
       </div>
-      <div className="relative">
-        <p>End Date</p>
-        <ReactDatePicker
-          selectsEnd
-          isClearable
-          minDate={new Date()}
-          dateFormat="yyyy/MM/dd"
-          showYearDropDown
-          showMonthDropDown
-          closeOnScroll={true}
-          className="cursor-pointer bg-teal-200 text-black p-2"
-        />
-      </div>
-      <div className="justify-center">
-        <button className="my-3 text-white p-2 bg-red-300">Filter Dates</button>
-      </div>
-    </div>
+    </>
   );
 }
 
