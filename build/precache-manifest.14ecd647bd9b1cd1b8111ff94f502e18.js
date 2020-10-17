6c03d4fcdb5a94820f835f5d31ba17eb971d30b7
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2020-10-17 14:45:55 +0530

    add production build

---
+++ build/precache-manifest.14ecd647bd9b1cd1b8111ff94f502e18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e9f56721b3ffb8e302fa4d60f50eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90fa5b9d7a9fd0810b15",
+    "url": "/static/css/2.33f7c388.chunk.css"
+  },
+  {
+    "revision": "f296cba0ac3e71fb49e5",
+    "url": "/static/css/main.8ea9ed34.chunk.css"
+  },
+  {
+    "revision": "90fa5b9d7a9fd0810b15",
+    "url": "/static/js/2.391be05b.chunk.js"
+  },
+  {
+    "revision": "1d9ed9265a32ea4776472f445cf2df78",
+    "url": "/static/js/2.391be05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f296cba0ac3e71fb49e5",
+    "url": "/static/js/main.663b61e6.chunk.js"
+  },
+  {
+    "revision": "66b327f18306c15d79d9",
+    "url": "/static/js/runtime-main.28cf7c2b.js"
+  },
+  {
+    "revision": "f3ef7c089aa29310e408d3df48590955",
+    "url": "/static/media/space-X-logo.f3ef7c08.svg"
+  }
+]);
